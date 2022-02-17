fcda862afa926e2c9c17b21263b392eb3e3e4816
guardofparadise <excelent91@mail.ru>
2022-02-17 10:27:00 +0200

    my custom code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Header from './components/header';
+import Banner from './components/banner';
 import "./styles/App.scss";
 
 function App() {
+
+  useEffect(() => {
+    let vh = window.innerHeight * .01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <div className='App'>
-      <h2>Hello this is your starter files </h2>
+      <Header />
+      <Banner />
     </div>
   );
 }
